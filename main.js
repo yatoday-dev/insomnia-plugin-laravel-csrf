dd143af8dddd1bac042baf0dd58fc618d181dc2a
alex <qwert2001qwert@gmail.com>
2025-05-13 22:03:46 +0200

    Add Laravel CSRF Insomnia plugin

---
+++ main.js
@@ -0,0 +1,40 @@
+module.exports.templateTags = [{
+    name: 'laravel_csrf',
+    displayName: 'Laravel CSRF',
+    description: 'Apply XSRF-TOKEN from cookie for X-XSRF-TOKEN request header',
+    args: [
+        {
+            displayName: 'CSRF Cookie Name',
+            type: 'string',
+            defaultValue: 'XSRF-TOKEN',
+            placeholder: 'It is "XSRF-TOKEN" by default in laravel',
+        }
+    ],
+    async run(context, cookieName = 'XSRF-TOKEN') {
+        const cookieJar = await this.getCookieJar(context);
+
+        const token = cookieJar.cookies?.find(cookie => cookie.key === cookieName);
+
+        if (token === undefined) {
+            throw new Error(`${cookieName} not found in cookies`);
+        }
+
+        return decodeURIComponent(token.value);
+    },
+
+    async getCookieJar(context) {
+        const { meta } = context;
+
+        if (!meta.requestId || !meta.workspaceId) {
+            throw new Error(`Request ID or workspace ID not found`);
+        }
+
+        const cookieJar = await context.util.models.cookieJar.getOrCreateForParentId(meta.workspaceId);
+
+        if (!cookieJar) {
+            throw new Error(`Cookie jar not found for ${meta.workspaceId}`);
+        }
+
+        return cookieJar;
+    },
+}];
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Insomnia Laravel CSRF Plugin
+
+The insomnia plugin for setting X-XSRF-TOKEN header in request from XSRF-TOKEN cookie in laravel applications.
+
+This repository is a fork of the original repository to support the latest version of Insomnia.
